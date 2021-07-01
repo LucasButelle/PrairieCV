f74025386f2fa86daddd6d2e8ae8d1319ef385df
LucasButelle <butelle.lucas.pro@gmail.com>
2021-07-01 09:42:34 +0200

    darkmode 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 function myFunction(body) {
     var element = document.body;
+    var button = document.getElementById("button");
     element.classList.toggle("dark-mode");
+
+    if(element.classList.contains("dark-mode")){
+        let msg = "Mode clair";
+        button.innerHTML = msg;
+    }else{
+        let msg = "Mode sombre";
+        button.innerHTML = msg;
+    }
 }
 
 var modal = document.getElementById("modal");
